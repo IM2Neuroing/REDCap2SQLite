efec1c4bec5fb350d3f7af8e6dc128dd0a4f09ea
marc <marc.jermann@fhnw.ch>
2024-04-15 13:22:23 +0200

    feat: Data Extraction setup
    - Updated patient ethnicity and race values in CSV file
    - Changed medication checkboxes to clearer labels
    - Added missing completion status in CSV
    - Removed unnecessary columns in LABELS CSV
    
    These changes improve data accuracy and clarity.

--- ETL/Extract/extract.py
+++ ETL/Extract/extract.py
@@ -1,5 +1,52 @@
+from PyUtilities.setupFunctions import read_config_file
+from redcap import Project
+import pandas as pd
+import logging
+
+
+CONFIG_FILE_PATH = 'config.json'
+
 def extract_data():
-  # Implement your logic to read data from the source (e.g., CSV file)
-  # Simulate reading data
-  data = {"data": [1, 2, 3]}
+  # load configuration file
+  config = read_config_file(CONFIG_FILE_PATH)
+
+  ## DATA EXTRACTION REDCAP to CSV
+  # Check if CSV file needs to be downloaded from REDCap
+  if config["extract_redcap"] is True:
+    # Run function to download data from REDCap
+    extract_redcap_data(config)
+
+  ## DATA EXTRACTION from CSV
+  # Logic to read data from CSV using pandas
+  data = pd.read_csv(config['extraction_path'])
   return data
+
+def extract_redcap_data(config):
+  """
+  Function to extract data from REDCap.
+  """
+  # Check if REDCap API token is provided
+  if config['redcap_api_token'] is None:
+    logging.error("No REDCap API token was specified in the config file")
+    exit()
+  # Check if REDCap URL is provided
+  if config['redcap_api_address'] is None:
+    logging.error("No REDCap URL was specified in the config file")
+    exit()
+  # Check if extraction path is provided
+  if config['extraction_path'] is None:
+    logging.error("No extraction path was specified in the config file")
+    exit()
+  
+  ## LOGIC to extract data from REDCap
+  api_url = config['redcap_api_address']
+  api_key = config['redcap_api_token']
+  project = Project(api_url, api_key)
+
+  # Download data from REDCap
+  data = project.export_records(raw_or_label="label")
+  # Save data to a file using pandas
+  df = pd.DataFrame(data)
+  df.to_csv(config['extraction_path'], index=False)
+  logging.info(f"Data extracted from REDCap: {df}")
+  return
